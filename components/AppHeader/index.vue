bdf726573f267f9cff0b3ccab62b5340c933b063
TJS-95 <tjs_95@163.com>
2020-08-23 10:37:23 +0800

    项目初始化

---
+++ components/AppHeader/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="AppHeader">
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="/">Damaio Jokes</b-navbar-brand>
+      <b-navbar-toggle target="jokesLinksBox"></b-navbar-toggle>
+      <b-collapse id="jokesLinksBox" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/">Home</b-nav-item>
+          <b-nav-item href="/jokes">Jokes</b-nav-item>
+          <b-nav-item href="/about">About</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<style>
+
+</style>
